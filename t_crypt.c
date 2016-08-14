ac90d33ffcb9382fdd3e20b5c7ef4fd271ee5919
Levi Durfee <levi.durfee@gmail.com>
2016-08-14 15:47:24 -0400

    added base64 encode/decode of key/iv

--- t_crypt.c
+++ t_crypt.c
@@ -10,6 +10,10 @@ int main(void) {
     int ct_len;
     int dt_len;
     
+    char *key_base64_e_output;
+    char *iv_base64_e_output;
+    unsigned char key_read_output[8192];
+    unsigned char iv_read_output[8192];
     unsigned char key[key_size];
     unsigned char iv[iv_size];
     unsigned char ciphertext[8192];
@@ -22,35 +26,64 @@ int main(void) {
     
     /* if key file exists */
     if( access( fn_key, F_OK ) != -1 ) {
-        FILE *key_fp;
-        key_fp = fopen(fn_key, "r");
-        fread(key, key_size, 1, key_fp);
-        printf("Key read\n");
-        fclose(key_fp);
+        FILE *key_fp_r;
+        key_fp_r = fopen(fn_key, "r");
+        if(key_fp_r == NULL) {
+            printf("ERROR OPENING KEY FILE\n");
+        }
+        
+        fread(key_read_output, 8192, 8192, key_fp_r);
+        if(ferror(key_fp_r)) {
+            printf("KEY READ ERROR\n");
+            return -1;
+        }
+        
+        if( Base64Decode(key_read_output, &key, &key_size) != 0) {
+            printf("key decode failed\n");
+            return -1;
+        }
+        
+        fclose(key_fp_r);
     } else {
         FILE *key_fp;
         key_fp = fopen(fn_key, "w");
+        
         lol_crypt_bytes(key_size, key);
-        fwrite(key, 1, key_size, key_fp);
-        printf("Key write\n");
+        Base64Encode(key, key_size, &key_base64_e_output);
+        fwrite(key_base64_e_output, 1, sizeof key_base64_e_output, key_fp);
+        
         fclose(key_fp);
     }
     /* if iv file exists */
     if( access( fn_iv, F_OK ) != -1 ) {
-        FILE *iv_fp;
-        iv_fp = fopen(fn_iv, "r");
-        fread(iv, iv_size, 1, iv_fp);
-        printf("Iv read\n");
-        fclose(iv_fp);
+        FILE *iv_fp_r;
+        iv_fp_r = fopen(fn_iv, "r");
+        
+        fread(iv_read_output, 8192, 8192, iv_fp_r);
+        if(ferror(iv_fp_r)) {
+            printf("IV READ ERROR\n");
+            return -1;
+        }
+        
+        if( Base64Decode(iv_read_output, &iv, &iv_size) != 0) {
+            printf("iv decode failed\n");
+            return -1;
+        }
+        
+        fclose(iv_fp_r);
     } else {
         FILE *iv_fp;
         iv_fp = fopen(fn_iv, "w");
+        
         lol_crypt_bytes(iv_size, iv);
-        fwrite(iv, 1, iv_size, iv_fp);
-        printf("Iv write\n");
+        
+        Base64Encode(iv, iv_size, &iv_base64_e_output);
+
+        fwrite(iv_base64_e_output, 1, sizeof iv_base64_e_output, iv_fp);
+        
         fclose(iv_fp);
     }
-    
+        
     unsigned char *plaintext =
                 (unsigned char *)"The quick brown fox jumps over the lazy dog";
     
