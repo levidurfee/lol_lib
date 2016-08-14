cb2dd22f1635183740b6b3f47a7b8b3158602095
Levi Durfee <levi.durfee@gmail.com>
2016-08-13 21:31:27 -0400

    update lol_primes to use lol_rand_entropy

--- libz/lol_primes.h
+++ libz/lol_primes.h
@@ -13,19 +13,19 @@
 #include <openssl/bn.h>
 #include <openssl/rand.h>
 #include <openssl/crypto.h>
+#include "lol_misc.h"
 
 void lol_primes(char *r_string, int bits) {
     BIGNUM *r;
-    /* thanks https://www.grc.com/otg/uheprng.htm */
-    static const char rnd_seed[] = "k5kgM<F)75OCSFSkSkrh)MIVVn+&uO$L \
-                                    I_r0Q3*Tp6wNg>}:[Ol3D!5/o`MhczJ8 \
-                                    ]'?qhu1U'}xyD7x5>$Re(JB.^0|;fGX_ \
-                                    82Tk7G)CNLCE+(^m|KFOMJa!0SFbdR@7 \
-                                    e.k-.g#[!5d*w<[O2_v#|;yKXM,{L?oE \
-                                    &SU_l}A,^f||A}',$f}e*cwRRG+C~N8< \
-                                    3|p#wNuG4r$lVD]]$!j)W--+sWUzBdQp \
-                                    t-h#oF&$O>|K]]wLxx2|fNm>-w[:N\v#";
-
+    
+    int e_num = 512;
+    char e[e_num];
+    lol_init_rand();
+    lol_rand_entropy(e_num, e);
+    
+    static const char *rnd_seed = NULL;
+    rnd_seed = e;
+    
     r = BN_new();
     RAND_seed(rnd_seed, sizeof rnd_seed);
     BN_generate_prime_ex(r, bits, 0, NULL, NULL, NULL);
