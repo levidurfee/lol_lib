7d5fb29c1bf167351ac3cb739136ee361c461b9d
Levi Durfee <levi.durfee@gmail.com>
2016-08-17 22:44:52 -0400

    fixed looping issue, added progress, and added colors lib

--- libz/lol_threads.h
+++ libz/lol_threads.h
@@ -12,9 +12,10 @@
 #include <unistd.h>
 #include <string.h>
 #include <pthread.h>
+#include "lol_colors.h"
 
 #ifndef LOL_NUM_THREADS
-#define LOL_NUM_THREADS 2
+#define LOL_NUM_THREADS 5
 #endif
 
 #ifndef LOL_THREADZ
@@ -30,11 +31,12 @@ typedef struct lol_threadz {
 
 int lol_threads(lol_threadz_s lts) {
     //int LOL_NUM_THREADS = sysconf(_SC_NPROCESSORS_ONLN);
-    int rc, cnt;
+    int rc, cnt, loops, progress;
     cnt = 0;
     long i;
     pthread_t threads[LOL_NUM_THREADS];
-    while(lts.loops >= cnt) {
+    loops = lts.loops / LOL_NUM_THREADS;
+    while(loops > cnt) {
         for(i=0;i<LOL_NUM_THREADS;i++) {
             if(lts.output == 1) {
                 printf("In main: creating thread %ld\n", i);
@@ -52,6 +54,8 @@ int lol_threads(lol_threadz_s lts) {
             }
         }
         cnt++;
+        progress = (cnt * 100) / loops;
+        printf(LOL_GREEN "Progress: %i percent" LOL_RESET "\n", progress);
     }
     pthread_exit(NULL);
     return 1;
