d11a54a8bea568ab4c1765ec7bead242c27dabcc
Levi Durfee <levi.durfee@gmail.com>
2016-09-27 22:03:37 -0400

    generate safe primes

--- libz/lol_primes.h
+++ libz/lol_primes.h
@@ -27,24 +27,27 @@ void lol_primes(char *r_string, int bits) {
     BIGNUM *r;
     
     int e_num = 512;
-    char e[e_num];
+    //char e[e_num];
+    char *e = (char *)malloc(e_num * sizeof(char));
     lol_init_rand();
     lol_rand_entropy(e_num, e);
+    
     /* below is for debugging */
-    /*
-    printf("Entropy: %s\n", e);
+    
+    /*printf("Entropy: %s\n", e);
     int e_len;
     e_len = strlen(e);
-    printf("%i\n", e_len);
-    */
+    printf("LEN: %i\n", e_len);*/
+    
     
     static const char *rnd_seed = NULL;
     rnd_seed = e;
     r = BN_new();
     RAND_seed(rnd_seed, sizeof rnd_seed);
-    BN_generate_prime_ex(r, bits, 0, NULL, NULL, NULL);
+    BN_generate_prime_ex(r, bits, 1, NULL, NULL, NULL);
     //strcpy(r_string, BN_bn2dec(r));
     sprintf(r_string, "%s", BN_bn2dec(r));
+    //free(e);
 
     BN_clear_free(r);
 }
