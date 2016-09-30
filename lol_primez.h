b676f01161126c81aad555fc5d57edc751ff105a
Levi Durfee <levi.durfee@gmail.com>
2016-09-30 15:17:14 -0400

    adding comments

--- lol_primez.h
+++ lol_primez.h
@@ -16,21 +16,22 @@ int l_rand_int(int min, int max) {
 int l_rand_entropy(size_t n_chars, char entropy[n_chars]) {
     int i, c;
     // add + 1 to account for \0
-    //char *p_buf = malloc((sizeof(char) * n_chars) + 1);
     char *p_buf = calloc(n_chars, sizeof(char) + 1);
+
+    // check and see if calloc was successful
     if(p_buf == 0) {
     	printf("Error: Out of memory\n");
     	return -1;
     }
     for(i=0;i<n_chars;i++) {
-        c = l_rand_int(33, 125); // ascii chars
-        //printf("%c", c);
-        //snprintf(p_buf, 1, "%c", c);
-        *p_buf = (char)c;
-        p_buf++;
-        //printf("%p\n", p_buf);
+        c = l_rand_int(33, 125); 	// ascii chars
+        *p_buf = (char)c; 			// cast the int to char
+        p_buf++;					// increment the pointer
     }
+    // copy the buffer to the char array
     strcpy(entropy, p_buf - i);
+
+    // free the memory. - i gets us back to the original location of memory
     free(p_buf - i);
     return 1;
 }
