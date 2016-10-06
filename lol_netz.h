9a481b57925e9af61ba75c5ee2fc851a149c32c5
Levi Durfee <levi.durfee@gmail.com>
2016-10-05 20:25:43 -0400

    updating internet files

--- lol_netz.h
+++ lol_netz.h
@@ -1,7 +1,7 @@
 #ifndef LOL_NETZ_H
 #define LOL_NETZ_H
 typedef struct {
-    struct sockaddr_in myaddr;      // defined in netinet/in.h
+    char myaddr;
 } lol_netz;
 
 lol_netz *lol_netz_new(lol_netz ln);
